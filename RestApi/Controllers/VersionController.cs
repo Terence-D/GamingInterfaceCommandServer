04b8c450f10829e031cf3c4dae3857a2c3b14936
Terence Doerksen <terence@coffeeshopstudio.ca>
2021-10-21 16:50:22 -0600

    added auth to version check

--- RestApi/Controllers/VersionController.cs
+++ RestApi/Controllers/VersionController.cs
@@ -8,14 +8,14 @@ namespace GIC.RestApi.Controllers
      * Used to validate the version of the server is what the client expects
      * */
     [ApiController]
+    [Authorize]
     [Route("api/[controller]")]
     public class VersionController : ControllerBase
     {
-        private const string API_VERSION = "2.1.0.0";
+        private const string API_VERSION = "2.1.0.1";
         /**
          * Returns current API version of this server
          * */
-        [AllowAnonymous]
         [HttpPost]
         public IActionResult Get()
         {
